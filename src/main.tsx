c6439e56be837bb81b1d60bb92a04035a5d10bdb
Ben Cacic <bencacic@gmail.com>
2026-05-07 00:12:07 -0600

    VERCEL: Added tracking and analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import Blokus from "./Blokus";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Blokus />
+    <Analytics />
   </React.StrictMode>
 );
